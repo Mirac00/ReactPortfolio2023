f4830821838f7019c1930a0ab838280d563c90c3
Sławomir Zając (Mirac00) <slawomir.zajac@example.com>
2025-05-14 00:19:05 +0200

    update langueswither

--- src/components/LangueSwitcher.js
+++ src/components/LangueSwitcher.js
@@ -1,38 +1,37 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import i18n from './utilities/i18n';
-import plFlag from '../images/plflag.png'; // Zaimportuj odpowiednie flagi
+import plFlag from '../images/plflag.png';
 import enFlag from '../images/enflag.jpg';
 import '../components/css/LanguageSwitcher.css';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-
-  const getFlag = () => {
-    switch(i18n.language) {
-      case 'pl': return plFlag;
-      case 'en': 
-      default: return enFlag;
-    }
+  const toggleLanguage = () => {
+    const newLanguage = i18n.language === 'pl' ? 'en' : 'pl';
+    i18n.changeLanguage(newLanguage);
   };
 
   return (
-    <div className="language-switcher">
-      <button 
-        onClick={() => changeLanguage(i18n.language === 'pl' ? 'en' : 'pl')}
-        className="language-button"
-        aria-label="Change language"
-      >
+    <div 
+      className="language-switcher" 
+      onClick={toggleLanguage}
+      aria-label="Change language"
+      role="button"
+      tabIndex={0}
+    >
+      <div className="language-flags-container">
         <img 
-          src={getFlag()} 
-          alt={i18n.language === 'pl' ? 'Polish flag' : 'English flag'} 
-          className="language-flag" 
+          src={plFlag} 
+          alt="Polish flag" 
+          className={`language-flag pl ${i18n.language === 'pl' ? 'active' : 'inactive'}`} 
         />
-      </button>
+        <img 
+          src={enFlag} 
+          alt="English flag" 
+          className={`language-flag en ${i18n.language === 'en' ? 'active' : 'inactive'}`} 
+        />
+      </div>
     </div>
   );
 };
